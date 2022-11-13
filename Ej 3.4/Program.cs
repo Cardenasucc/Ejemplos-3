581165f290c6363cc9632838c26c44ec32ce0021
Adriana <Adriana@LAPTOP-P9HSBAIV>
2022-11-12 23:51:01 -0500

    Agregar archivos de proyecto.

---
+++ Ej 3.4/Program.cs
@@ -0,0 +1,30 @@
+﻿//ejemplos 3
+using System;
+namespace Ejercicio3_4
+{
+    class ejercicio
+    {
+        static void Main()
+        {
+
+            double descuento;
+            double precio;
+            double total;
+            Console.Write("Ingresa el precio del traje: ");
+            Console.WriteLine("");
+            precio = Convert.ToInt32(Console.ReadLine());
+            if (precio > 2500)
+            {
+                descuento = precio * .15;
+            }
+            else
+            {
+                descuento = precio * .08;
+            }
+            total = precio - descuento;
+            Console.Write("El precio final por el traje es: $" + total);
+            Console.WriteLine("");
+            Console.Write("El descuento aplicado es: $" + descuento);
+        }
+    }
+}
